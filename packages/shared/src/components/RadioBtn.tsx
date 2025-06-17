eb70c71ee786a1a25927cc350d7be82a78426b79
gikdev <m.m.bhrm85@gmail.com>
2025-06-17 22:13:55 +0330

    Fixed `<Radio />` & `<Switch />`...

--- packages/shared/src/components/RadioBtn.tsx
+++ packages/shared/src/components/RadioBtn.tsx
@@ -1,21 +1,27 @@
+import styled from "@master/styled.react"
 import { forwardRef } from "react"
 
 export const Radio = forwardRef<HTMLInputElement, React.ComponentPropsWithoutRef<"input">>(
   ({ checked, className = "", ...props }, ref) => {
+    const StyledRing = styled.span`
+      w-5 h-5 rounded-full 
+      inline-block border transition-colors
+      border-slate-7 bg-slate-3
+      peer-checked:border-brand-9
+      peer-checked:bg-brand-3
+    `
+    const StyledCenter = styled.span`
+      h-3 w-3 rounded-full absolute
+      bg-slate-3 top-1 left-1
+      peer-checked:bg-brand-9
+      transition-colors inline-block 
+    `
+
     return (
-      <label className={`relative inline-flex items-center cursor-pointer ${className}`}>
-        <input type="radio" className="sr-only" checked={checked} ref={ref} {...props} />
-        <span
-          className={`
-            w-5 h-5 rounded-full border transition-all flex 
-            ${checked ? "border-brand-9" : "border-slate-7"}
-            ${checked ? "bg-brand-3" : "bg-slate-3"}
-            items-center justify-center
-            after:size-3 after:rounded-full
-            ${checked ? "after:bg-brand-9" : "after:bg-slate-3"}
-            after:transition-all after:inline-block 
-          `}
-        />
+      <label className={`relative w-5 h-5 cursor-pointer ${className}`}>
+        <input type="radio" className="sr-only peer" checked={checked} ref={ref} {...props} />
+        <StyledRing />
+        <StyledCenter />
       </label>
     )
   },
