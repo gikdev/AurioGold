2bb4727198fd7e1bb3ab9da390ac1222b6acb42a
gikdev <m.m.bhrm85@gmail.com>
2025-08-31 11:39:15 +0330

    Refactored styles of `<TitledCard />`

--- packages/shared/src/components/TitledCard.tsx
+++ packages/shared/src/components/TitledCard.tsx
@@ -1,19 +1,18 @@
-import styled from "@master/styled.react"
 import type { Icon } from "@phosphor-icons/react"
 import type { HTMLAttributes, ReactNode } from "react"
 import { cn } from "#shared/helpers"
 import { Heading } from "./Heading"
+import { cx } from "#shared/lib/cva.config"
 
-const StyledContainer = styled.div`
+const containerStyles = cx(`
   flex flex-col gap-8 bg-slate-2 border border-slate-6
-  p-4 rounded-md max-w-160 w-full mx-auto transition-all
-`
-const classes = {
-  header: `
-    flex flex-wrap gap-1 justify-start
-    border-b border-slate-6 pb-4 transition-all
-  `,
-}
+  p-4 rounded-md max-w-160 w-full mx-auto
+`)
+
+const headerStyles = cx(`
+  flex flex-wrap gap-1 justify-start
+  border-b border-slate-6 pb-4
+`)
 
 interface TitledCardProps extends HTMLAttributes<HTMLElement> {
   children: ReactNode
@@ -29,11 +28,12 @@ export function TitledCard({
   title,
   headerClassName = "",
   titleSlot,
+  className,
   ...other
 }: TitledCardProps) {
   return (
-    <StyledContainer {...other}>
-      <div className={cn(classes.header, headerClassName)}>
+    <div className={cn(containerStyles, className)} {...other}>
+      <div className={cn(headerStyles, headerClassName)}>
         <div className="flex items-center gap-1">
           <Icon size={24} />
           <Heading size={1}>{title}</Heading>
@@ -43,6 +43,6 @@ export function TitledCard({
       </div>
 
       {children}
-    </StyledContainer>
+    </div>
   )
 }
