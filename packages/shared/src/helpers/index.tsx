de5f995ead900a25b37c2af919fedc8d6cf882fd
gikdev <m.m.bhrm85@gmail.com>
2025-08-31 18:42:55 +0330

    WIP: Trying to fix 401 -> kick the user out...

--- packages/shared/src/helpers/index.tsx
+++ packages/shared/src/helpers/index.tsx
@@ -86,27 +86,35 @@ export function createControlledAsyncToast<T = void>(
   }
 }
 
-export function parseError(data: unknown, msg?: string) {
-  if (typeof data === "string") {
+export function parseError(error: unknown, msg?: string) {
+  if (typeof error === "string") {
     try {
-      const parsed = JSON.parse(data)
+      const parsed = JSON.parse(error)
 
       if (typeof parsed === "object" && parsed !== null) {
         if ("msg" in parsed && typeof parsed.msg === "string") return parsed.msg
         if ("message" in parsed && typeof parsed.message === "string") return parsed.message
         if ("name" in parsed && typeof parsed.name === "string") return parsed.name
         if ("title" in parsed && typeof parsed.title === "string") return parsed.title
+        if ("status" in parsed && typeof parsed.status === "number") {
+          if (parsed.status === 401)
+            return "دسترسی نامعتبر هست، لطفا از حساب خارج شده و دوباره وارد شوید"
+        }
       }
     } catch {
-      return data
+      return error
     }
   }
 
-  if (typeof data === "object" && data !== null) {
-    if ("msg" in data && typeof data.msg === "string") return data.msg
-    if ("message" in data && typeof data.message === "string") return data.message
-    if ("name" in data && typeof data.name === "string") return data.name
-    if ("title" in data && typeof data.title === "string") return data.title
+  if (typeof error === "object" && error !== null) {
+    if ("msg" in error && typeof error.msg === "string") return error.msg
+    if ("message" in error && typeof error.message === "string") return error.message
+    if ("name" in error && typeof error.name === "string") return error.name
+    if ("title" in error && typeof error.title === "string") return error.title
+    if ("status" in error && typeof error.status === "number") {
+      if (error.status === 401)
+        return "دسترسی نامعتبر هست، لطفا از حساب خارج شده و دوباره وارد شوید"
+    }
   }
 
   return msg || "خطایی رخ داد!"
